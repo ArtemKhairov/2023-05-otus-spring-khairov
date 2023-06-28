88779a26d2d60127b24fd878586695a83c363d94
ArtemKhairov <artemxairoff@icloud.com>
2023-06-28 20:51:45 +0700

    add unit test for CSVReaderDAO

---
+++ hw-01/src/test/java/ru/otus/tests/CSVReaderDAOTest.java
@@ -0,0 +1,33 @@
+package ru.otus.tests;
+
+import ru.otus.spring.dao.CSVReaderDAO;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@DisplayName("CSVReaderDAOTest")
+public class CSVReaderDAOTest {
+    private static final int QUESTIONS_COUNT = 5;
+    private static final String EXIST_FILE = "questions.csv";
+    private static final String NOT_EXIST = "not-exist-file.csv";
+
+    @DisplayName("Test with Existing File")
+    @Test
+    void existFileTest() {
+        var questionsDao = new CSVReaderDAO(EXIST_FILE);
+        var questions = questionsDao.getAll();
+        assertEquals(QUESTIONS_COUNT, questions.size());
+    }
+
+    @DisplayName("Test with Non existent file")
+    @Test
+    void nonExistentFileTest() {
+        var questionsDao = new CSVReaderDAO(NOT_EXIST);
+        var ex = assertThrows(RuntimeException.class, questionsDao::getAll);
+        assertEquals(String.format("Could not open questions file"), ex.getMessage());
+    }
+
+
+}
